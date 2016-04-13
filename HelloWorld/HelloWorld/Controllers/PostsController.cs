52bcec66d637bacb38297feb40241eb74ade7cca
Michael S. Collier <buckeye01@hotmail.com>
2016-04-13 00:03:50 -0400

    Adding initial logic for Posts.

---
+++ HelloWorld/HelloWorld/Controllers/PostsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorld.Models;
+
+namespace HelloWorld.Controllers
+{
+    public class PostsController : Controller
+    {
+        private BloggingContext db = new BloggingContext();
+
+        // GET: Posts
+        public async Task<ActionResult> Index()
+        {
+            var posts = db.Posts.Include(p => p.Blog);
+            return View(await posts.ToListAsync());
+        }
+
+        // GET: Posts/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = await db.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // GET: Posts/Create
+        public ActionResult Create()
+        {
+            ViewBag.BlogId = new SelectList(db.Blogs, "BlogId", "Name");
+            return View();
+        }
+
+        // POST: Posts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "PostId,Title,Content,BlogId")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Posts.Add(post);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BlogId = new SelectList(db.Blogs, "BlogId", "Name", post.BlogId);
+            return View(post);
+        }
+
+        // GET: Posts/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = await db.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BlogId = new SelectList(db.Blogs, "BlogId", "Name", post.BlogId);
+            return View(post);
+        }
+
+        // POST: Posts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "PostId,Title,Content,BlogId")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(post).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BlogId = new SelectList(db.Blogs, "BlogId", "Name", post.BlogId);
+            return View(post);
+        }
+
+        // GET: Posts/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = await db.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // POST: Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Post post = await db.Posts.FindAsync(id);
+            db.Posts.Remove(post);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
